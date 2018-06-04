8931ae071251f48e839cf370abff9c8debdc19b9
RhiaKe <rhiakaulitzke@gmail.com>
2018-06-04 08:28:53 -0600

    made header sticky

--- src/components/Freebies.js
+++ src/components/Freebies.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Freebies = () =>
   <div>
-  	<h2>Coming Soon!</h2>
+  	<h2 className="CStitle">Coming Soon!</h2>
   </div>
 
 export default Freebies;
--- src/components/Inspiration.js
+++ src/components/Inspiration.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Inspiration = () =>
   <div>
-  	<h2>Coming Soon!</h2>
+  	<h2 className="CStitle">Coming Soon!</h2>
   </div>
 
 export default Inspiration;
